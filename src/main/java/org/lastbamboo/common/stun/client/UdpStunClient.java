484138b18416dce2d2d20d18d0c9902b064dd1fd
adamfisk <a@littleshoot.org>
2007-06-04 17:05:17 +0000

    fixed imports
    git-svn-id: http://svn.littleshoot.org/svn/littleshoot/trunk/common/stun/client@455 8d7aff80-242d-0410-bcee-ed6f15385b3d

--- src/main/java/org/lastbamboo/common/stun/client/UdpStunClient.java
+++ src/main/java/org/lastbamboo/common/stun/client/UdpStunClient.java
@@ -5,7 +5,6 @@ import java.net.InetSocketAddress;
 import java.net.UnknownHostException;
 import java.util.Arrays;
 
-import org.apache.commons.lang.ArrayUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.mina.common.ConnectFuture;
