e17380d64ffc3cbad547fc5a4b24aec1a26fc399
adamfisk <a@littleshoot.org>
2008-10-31 04:06:34 +0000

    Simplified STUN client code for clarity
    git-svn-id: http://svn.littleshoot.org/svn/littleshoot/trunk/common/stun/client@4074 8d7aff80-242d-0410-bcee-ed6f15385b3d

--- src/main/java/org/lastbamboo/common/stun/client/UdpStunClient.java
+++ src/main/java/org/lastbamboo/common/stun/client/UdpStunClient.java
@@ -2,40 +2,87 @@ package org.lastbamboo.common.stun.client;
 
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
+import java.net.UnknownHostException;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import org.apache.commons.id.uuid.UUID;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+import org.apache.mina.common.ByteBuffer;
+import org.apache.mina.common.ConnectFuture;
 import org.apache.mina.common.ExecutorThreadModel;
 import org.apache.mina.common.IoConnector;
 import org.apache.mina.common.IoHandler;
+import org.apache.mina.common.IoServiceListener;
 import org.apache.mina.common.IoSession;
+import org.apache.mina.common.SimpleByteBufferAllocator;
+import org.apache.mina.filter.codec.ProtocolCodecFactory;
+import org.apache.mina.filter.codec.ProtocolCodecFilter;
 import org.apache.mina.transport.socket.nio.DatagramConnector;
 import org.apache.mina.transport.socket.nio.DatagramConnectorConfig;
+import org.lastbamboo.common.stun.stack.StunIoHandler;
+import org.lastbamboo.common.stun.stack.StunProtocolCodecFactory;
+import org.lastbamboo.common.stun.stack.message.BindingErrorResponse;
 import org.lastbamboo.common.stun.stack.message.BindingRequest;
+import org.lastbamboo.common.stun.stack.message.BindingSuccessResponse;
+import org.lastbamboo.common.stun.stack.message.ConnectErrorStunMessage;
 import org.lastbamboo.common.stun.stack.message.NullStunMessage;
 import org.lastbamboo.common.stun.stack.message.StunMessage;
+import org.lastbamboo.common.stun.stack.message.StunMessageVisitor;
+import org.lastbamboo.common.stun.stack.message.StunMessageVisitorAdapter;
+import org.lastbamboo.common.stun.stack.message.StunMessageVisitorFactory;
+import org.lastbamboo.common.stun.stack.transaction.StunTransactionListener;
 import org.lastbamboo.common.stun.stack.transaction.StunTransactionTracker;
+import org.lastbamboo.common.stun.stack.transaction.StunTransactionTrackerImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
- * STUN client implementation for ICE UDP. 
+ * Abstract STUN client.  Subclasses typically define transports.
  */
-public class UdpStunClient extends AbstractStunClient
+public class UdpStunClient implements StunClient, StunTransactionListener
     {
-    
-    private final Log LOG = LogFactory.getLog(UdpStunClient.class);
+
+    private static final Logger LOG = 
+        LoggerFactory.getLogger(UdpStunClient.class);
     
     /**
-     * Creates a new STUN client for ICE processing.  This client is capable
-     * of obtaining "server reflexive" and "host" candidates.  We don't use
-     * relaying for UDP, so this does not currently support generating
-     * "relayed" candidates.
+     * The default STUN port.
      */
-    public UdpStunClient()
-        {
-        super();
-        }
+    private static final int STUN_PORT = 3478;
         
+    private final Collection<IoServiceListener> m_ioServiceListeners =
+        new LinkedList<IoServiceListener>();
+
+    /**
+     * This is the address of the STUN server to connect to.
+     */
+    protected final InetSocketAddress m_stunServerAddress;
+    
+    private final IoHandler m_ioHandler;
+
+    protected final Map<UUID, StunMessage> m_idsToResponses =
+        new ConcurrentHashMap<UUID, StunMessage>();
+
+    protected InetSocketAddress m_localAddress;
+
+    /**
+     * Just keeps track of the current connection 5-tuple so we don't try
+     * to connect to the host we're already connected to.
+     */
+    private IoSession m_currentIoSession;
+
+    protected final StunTransactionTracker<StunMessage> m_transactionTracker;
+
+    private final InetSocketAddress m_originalLocalAddress;
+
+    private final Collection<IoSession> m_sessions = 
+        new LinkedList<IoSession>();
+    
     /**
      * Creates a new STUN client for ICE processing.  This client is capable
      * of obtaining "server reflexive" and "host" candidates.  We don't use
@@ -43,39 +90,201 @@ public class UdpStunClient extends AbstractStunClient
      * "relayed" candidates.
      * 
      * @param transactionTracker The transaction tracker to use.
+     * @param ioHandler The {@link IoHandler} to use.
      */
     public UdpStunClient(
         final StunTransactionTracker<StunMessage> transactionTracker,
         final IoHandler ioHandler)
         {
-        super(transactionTracker, ioHandler);
+        this (null, pickStunServerInetAddress(), transactionTracker, ioHandler);
         }
     
     /**
      * Creates a new STUN client that connects to the specified STUN server.
      * 
-     * @param stunServerAddress The address of the STUN server to connect 
-     * to.  This connects on the default port and is primarily used for 
-     * testing.
+     * @param stunServerAddress The address of the STUN server to connect to.
      */
     public UdpStunClient(final InetAddress stunServerAddress)
         {
-        super(stunServerAddress);
+        this (null, stunServerAddress, null, null);
         }
-
+    
     /**
-     * Creates a new UDP STUN client that binds to the specified local 
-     * address.
-     * 
-     * @param localAddress The local address to bind to. 
+     * Creates a new STUN client that connects to the specified STUN server.
      */
-    public UdpStunClient(final InetSocketAddress localAddress)
+    public UdpStunClient()
         {
-        super(localAddress);
+        this (pickStunServerInetAddress());
+        }
+    
+    /**
+     * Creates a new STUN client that connects to the specified STUN server.
+     * 
+     * @param stunServerAddress The address of the STUN server to connect to.
+     * @param connectTimeout The timeout to wait for connections.
+     */
+    private UdpStunClient(final InetSocketAddress localAddress,
+        final InetAddress stunServerAddress,
+        final StunTransactionTracker<StunMessage> transactionTracker, 
+        final IoHandler ioHandler)
+        {
+        if (stunServerAddress == null)
+            {
+            LOG.error("Null STUN server");
+            throw new NullPointerException("Null STUN server");
+            }
+        ByteBuffer.setUseDirectBuffers(false);
+        ByteBuffer.setAllocator(new SimpleByteBufferAllocator());
+        m_originalLocalAddress = localAddress;
+        if (transactionTracker == null)
+            {
+            this.m_transactionTracker = new StunTransactionTrackerImpl();
+            }
+        else
+            {
+            this.m_transactionTracker = transactionTracker;
+            }
+        
+        m_stunServerAddress = 
+            new InetSocketAddress(stunServerAddress, STUN_PORT);
+        
+        if (ioHandler == null)
+            {
+            final StunMessageVisitorFactory messageVisitorFactoryToUse = 
+                new StunClientMessageVisitorFactory(this.m_transactionTracker);
+            m_ioHandler = new StunIoHandler(messageVisitorFactoryToUse);
+            }
+        else
+            {
+            m_ioHandler = ioHandler;
+            }
+        }
+    
+    public void connect()
+        {
+        final IoSession session = 
+            connect(m_originalLocalAddress, m_stunServerAddress); 
+        
+        // We set the local address here because the original could be null
+        // to bind to an ephemeral port.
+        this.m_localAddress = (InetSocketAddress) session.getLocalAddress();
+        }
+    
+    protected final IoSession connect(final InetSocketAddress localAddress, 
+        final InetSocketAddress stunServerAddress)
+        {
+        // We can't connect twice to the same 5-tuple, so check to verify we're
+        // not reconnecting to the remote host we're already connected to.
+        if (this.m_currentIoSession != null && 
+            this.m_currentIoSession.getRemoteAddress().equals(stunServerAddress))
+            {
+            return this.m_currentIoSession;
+            }
+        
+        final ProtocolCodecFactory codecFactory = 
+            new StunProtocolCodecFactory();
+        final ProtocolCodecFilter stunFilter = 
+            new ProtocolCodecFilter(codecFactory);
+        
+        final IoConnector connector = createConnector();
+        connector.getFilterChain().addLast("stunFilter", stunFilter);
+        
+        if (this.m_ioServiceListeners.isEmpty())
+            {
+            LOG.debug("No service listeners for: {}",
+                getClass().getSimpleName());
+            }
+        synchronized (this.m_ioServiceListeners)
+            {
+            for (final IoServiceListener sl : this.m_ioServiceListeners)
+                {
+                connector.addListener(sl);
+                }
+            }
+        LOG.debug("Connecting to: {}", stunServerAddress);
+        final ConnectFuture cf = 
+            connector.connect(stunServerAddress, localAddress, m_ioHandler);
+        LOG.debug("About to join");
+        cf.join();
+        LOG.debug("Connected to: {}", stunServerAddress);
+        final IoSession session = cf.getSession();
+        this.m_sessions.add(session);
+        this.m_currentIoSession = session;
+        return session;
+        }
+    
+    public InetSocketAddress getHostAddress()
+        {
+        return m_localAddress;
+        }
+    
+    public InetAddress getStunServerAddress()
+        {
+        return this.m_stunServerAddress.getAddress();
         }
 
-    @Override
-    protected IoConnector createConnector()
+    protected void waitIfNoResponse(final StunMessage request, 
+        final long waitTime)
+        {
+        LOG.debug("Waiting "+waitTime+" milliseconds...");
+        if (waitTime == 0L) return;
+        if (!m_idsToResponses.containsKey(request.getTransactionId()))
+            {
+            try
+                {
+                LOG.debug("Actually waiting...");
+                request.wait(waitTime);
+                }
+            catch (final InterruptedException e)
+                {
+                LOG.error("Unexpected interrupt", e);
+                }
+            }
+        }
+
+    public Object onTransactionFailed(final StunMessage request,
+        final StunMessage response)
+        {
+        return notifyWaiters(request, response);
+        }
+    
+    public Object onTransactionSucceeded(final StunMessage request, 
+        final StunMessage response)
+        {
+        return notifyWaiters(request, response);
+        }
+
+    private Object notifyWaiters(StunMessage request, StunMessage response)
+        {
+        synchronized (request)
+            {
+            this.m_idsToResponses.put(request.getTransactionId(), response);
+            request.notify();
+            }
+        return null;
+        }
+    
+    public final void addIoServiceListener(
+        final IoServiceListener serviceListener)
+        {
+        LOG.debug("Adding service listener for: {}", this);
+        this.m_ioServiceListeners.add(serviceListener);
+        }
+
+    public void close()
+        {
+        LOG.debug("Closing sessions...");
+        synchronized (m_sessions)
+            {
+            for (final IoSession session : m_sessions)
+                {
+                LOG.debug("Closing: {}", session);
+                session.close();
+                }
+            }
+        }
+    
+    private IoConnector createConnector()
         {
         final DatagramConnector connector = new DatagramConnector();
         final DatagramConnectorConfig cfg = connector.getDefaultConfig();
@@ -83,6 +292,41 @@ public class UdpStunClient extends AbstractStunClient
         cfg.setThreadModel(
             ExecutorThreadModel.getInstance(getClass().getSimpleName()));
         return connector;
+        }
+    
+    public InetSocketAddress getServerReflexiveAddress()
+        {
+        final BindingRequest br = new BindingRequest();
+        final StunMessage message = write(br, this.m_stunServerAddress);
+        final StunMessageVisitor<InetSocketAddress> visitor = 
+            new StunMessageVisitorAdapter<InetSocketAddress>()
+            {
+            @Override
+            public InetSocketAddress visitBindingSuccessResponse(
+                final BindingSuccessResponse response)
+                {
+                return response.getMappedAddress();
+                }
+
+            @Override
+            public InetSocketAddress visitBindingErrorResponse(
+                final BindingErrorResponse response)
+                {
+                LOG.warn("Received Binding Error Response: "+response);
+                return null;
+                }
+
+            @Override
+            public InetSocketAddress visitConnectErrorMesssage(
+                final ConnectErrorStunMessage error)
+                {
+                LOG.warn("Received ICMP error: {}", error);
+                return null;
+                }
+            };
+          
+        final InetSocketAddress isa = message.accept(visitor);
+        return isa;
         }
  
     public StunMessage write(final BindingRequest request, 
@@ -160,5 +404,35 @@ public class UdpStunClient extends AbstractStunClient
         // We don't map ports for clients (only for classes that also accept
         // incoming connections).
         return false;
+        }
+    
+    private static InetAddress pickStunServerInetAddress()
+        {
+        final List<String> servers = 
+            Arrays.asList(
+                new String[] 
+                    {
+                    //"stunserver.org",
+                    "stun01.sipphone.com",
+                    "stun.ideasip.com",
+                    "stun.ekiga.net",
+                    }
+                );
+        Collections.shuffle(servers);
+        for (final String server : servers)
+            {
+            try
+                {
+                return InetAddress.getByName(server);
+                }
+            catch (final UnknownHostException e)
+                {
+                LOG.error("Could not lookup host: "+server, e);
+                }
+            }
+        
+        LOG.error("All hosts failed!!: {}", servers);
+        throw new IllegalArgumentException("Could not lookup host.  " +
+            "No network?  Tried servers: {}" + servers);
         }
     }
